ffff901777a7053135bc0b7a0385f371a160f0af
kwakdo <vv05044@gmail.com>
2022-08-08 15:33:38 +0900

    first commit

---
+++ src/main/java/com/jeonwoojo/plendar/PlendarApplication.java
@@ -0,0 +1,13 @@
+package com.jeonwoojo.plendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlendarApplication.class, args);
+	}
+
+}
