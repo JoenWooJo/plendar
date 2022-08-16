8ed9320640b8fa494579b41928097e3532cebc16
yujin <yujin@yujinui-MacBookAir.local>
2022-08-16 17:20:42 +0900

    칸반, 버튼 수정

--- frontend/src/component/fileSharing/FileSharing.js
+++ frontend/src/component/fileSharing/FileSharing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SiteLayout from '../../layout/SiteLayout';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const fileSharing = () => {
     return (
@@ -42,11 +43,13 @@ const fileSharing = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-file-earmark-arrow-up" viewBox="0 0 16 16">
                     <path d="M8.5 11.5a.5.5 0 0 1-1 0V7.707L6.354 8.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 7.707V11.5z"/>
                     <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-                    </svg><br></br>
+                    </svg><hr/>
 
-                    <Link to="#" style={{ textDecoration: "none" }}>
-                    <button type="button" className=" mt-4 mr-2 btn btn-secondary">클릭해서 파일 업로드 하기</button>
-                    </Link>
+                    <Button variant="outlined" component="label">
+                        File Upload
+                        <input hidden accept="image/*" multiple type="file" />
+                    </Button>
+                    
                     </div>
                 </div>
             </div>
--- frontend/src/component/kanbanboard/Kanban.js
+++ frontend/src/component/kanbanboard/Kanban.js
@@ -1,12 +1,109 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Board, { moveCard } from "@asseinfo/react-kanban";
+import "@asseinfo/react-kanban/dist/styles.css";
 import SiteLayout from '../../layout/SiteLayout';
+//import './style.css';
+
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Backlog",
+      backgroundColor: "#fff",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Doing",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Q&A",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Production",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
+
+
+function UncontrolledBoard() {
+  return (
+    <Board
+      allowRemoveLane
+      allowRenameColumn
+      allowRemoveCard
+      onLaneRemove={console.log}
+      onCardRemove={console.log}
+      onLaneRename={console.log}
+      initialBoard={board}
+      allowAddCard={{ on: "top" }}
+      onNewCardConfirm={(draftCard) => ({
+        id: new Date().getTime(),
+        ...draftCard
+      })}
+      onCardNew={console.log}
+    />
+  );
+}
+
 
 const kanban = () => {
     return (
         
         <SiteLayout>
-            <div className="col-xl-10">
-            <h1>칸반보드</h1>
+            <div className="ml-4">
+            <h4>도훈이의 미니프로젝트</h4>
+                <UncontrolledBoard />
            </div>
         </SiteLayout>
         
