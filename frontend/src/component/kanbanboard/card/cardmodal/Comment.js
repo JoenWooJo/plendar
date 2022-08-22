bff2206f7e1701e8c979addf21c91eb501f406cb
yujin <yujin@yujinui-MacBookAir.local>
2022-08-22 16:02:25 +0900

    kanbanComment

--- frontend/src/component/kanbanboard/card/cardmodal/Comment.js
+++ frontend/src/component/kanbanboard/card/cardmodal/Comment.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Card } from 'react-bootstrap';
 import Button from '@mui/material/Button';
+import moment from 'moment';
+import 'moment/locale/ko';
 
 const Comment = () => {
+    let [userName] = useState('김채원');
+    const [comment, setComment] = useState('');
+    const [feedComments, setFeedComments] = useState([]);
+    const [isValid, setIsValid] = useState(false);
+    const nowTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    let post = e =>{
+        setFeedComments([...feedComments, comment]);
+        setComment('');
+    };
+
+    const CommentList = ({name, comment}) => {
+        return(
+            <div>{name} : {comment } 
+            <div className='float-right'> {nowTime}</div>
+            </div>  
+        )
+    }
+
     return (
         <div>
             comment
             <Card style={{ height: "200px" }} >
-                
-                <Card body >
-                    곽도훈: 점심 뭐드실?
-                    <div className='float-right'>
-                        2022/08/22
-                    </div>
+            {feedComments.map((commentArr, i)=>{
+                return(
+                <Card body key={i}>
+                       <CommentList name={userName} comment={commentArr}/>
                 </Card>
-
-                <Card body >
-                    김채원: 마라샹궈먹으러 가자
-                    <div className='float-right'>
-                        2022/08/22
-                    </div>
-                </Card>
-            </Card>
+                );
+            })}
+              </Card>
 
             {/* 코멘트 입력 */}
             <hr />
@@ -30,10 +44,27 @@ const Comment = () => {
                     className="col-xl-10 mb-3 ml-4 "
                     controlId="exampleForm.ControlTextarea1"
                 >
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} 
+                        onChange={e =>{
+                            setComment(e.target.value);
+                        }}
+                        onKeyUp={e=>{
+                            e.target.value.length > 0
+                            ?setIsValid(true)
+                            :setIsValid(false);
+                        }}
+                        value={comment}
+                    />
                 </Form.Group>
 
-                <Button style={{ height: "50px" }} className="col-xl-1 mt-3" variant="primary">
+                <Button style={{ height: "50px" }} className={"col-xl-1 mt-3",
+                        comment.length > 0
+                            ?'submitCommentActive'
+                            :'submitCommentInactive'}
+                             variant="primary"
+                             onClick={post}
+                             disabled={isValid?false:true}
+                        >
                     확인
                 </Button>
             </div>
