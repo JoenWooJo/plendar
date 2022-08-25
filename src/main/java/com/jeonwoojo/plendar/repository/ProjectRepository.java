48bfed4696e198c46db435bb4a2ccffe25209531
chaewon <rlacodnjs98@naver.com>
2022-08-25 15:10:52 +0900

    프로젝트 생성 - Deck default 추가

--- src/main/java/com/jeonwoojo/plendar/repository/ProjectRepository.java
+++ src/main/java/com/jeonwoojo/plendar/repository/ProjectRepository.java
@@ -1,5 +1,6 @@
 package com.jeonwoojo.plendar.repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.jeonwoojo.plendar.vo.DeckVo;
 import com.jeonwoojo.plendar.vo.ProjectVo;
 import com.jeonwoojo.plendar.vo.UserVo;
 
@@ -20,13 +22,16 @@ public class ProjectRepository {
 		return sqlSession.selectList("project.findUser");
 	}
 
-	public ProjectVo createProject(ProjectVo projectVo) {
+	public ProjectVo createProject(ProjectVo projectVo, UserVo authUser) {
 		sqlSession.insert("project.createProject", projectVo);
 		
 		List<UserVo> list = projectVo.getMember();
 		
 		HashMap<String, Object> map = new HashMap<>();
 		map.put("projectNo", projectVo.getNo());
+		map.put("authUser", authUser);
+		
+		sqlSession.insert("project.insertReader", map);
 		
 		for (int i=0;i<list.size();i++) {
 			if(list.get(i).getPermission() == null) {
@@ -40,6 +45,32 @@ public class ProjectRepository {
 		}
 		
 		sqlSession.insert("chat.chatRoomCreate", projectVo);
+		
+		ArrayList<DeckVo> defaultDeck = new ArrayList<DeckVo>();
+		
+		DeckVo toDo = new DeckVo();
+		toDo.setProjectNo(projectVo.getNo());
+		toDo.setTitle("ToDo");
+		toDo.setSequence(1);
+		defaultDeck.add(toDo);
+		
+		DeckVo doing = new DeckVo();
+		doing.setProjectNo(projectVo.getNo());
+		doing.setTitle("Doing");
+		doing.setSequence(2);
+		defaultDeck.add(doing);
+		
+		DeckVo done = new DeckVo();
+		done.setProjectNo(projectVo.getNo());
+		done.setTitle("Done");
+		done.setSequence(3);
+		defaultDeck.add(done);
+		
+		for (int i=0;i<defaultDeck.size();i++) {
+			map.put("deck", defaultDeck.get(i));
+			sqlSession.insert("deck.insertDefaultDeck", map);
+			map.remove("deck");
+		}
 		
 		return projectVo;
 	}
