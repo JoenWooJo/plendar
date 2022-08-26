080dcac0657dc997cc8f47559f4d1b9895f73f90
yujin <yujin@localhost>
2022-08-26 15:57:14 +0900

    deck 추가, 수

--- src/main/java/com/jeonwoojo/plendar/controller/DeckController.java
+++ src/main/java/com/jeonwoojo/plendar/controller/DeckController.java
@@ -1,17 +1,25 @@
 package com.jeonwoojo.plendar.controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jeonwoojo.plendar.dto.JsonResult;
 import com.jeonwoojo.plendar.security.Auth;
+import com.jeonwoojo.plendar.security.AuthUser;
 import com.jeonwoojo.plendar.service.DeckService;
 import com.jeonwoojo.plendar.vo.DeckVo;
+import com.jeonwoojo.plendar.vo.ProjectVo;
+import com.jeonwoojo.plendar.vo.UserVo;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:9090")
@@ -23,12 +31,29 @@ public class DeckController {
 	
 	
 	@Auth
-	@GetMapping("/find")
-	public ResponseEntity<JsonResult> findDeck(DeckVo deckVo) {
+	@GetMapping("/find/{projectNo}")
+	public ResponseEntity<JsonResult> findDeck(@PathVariable("projectNo") Long projectNo) {
+		System.out.println("deckfind"+ projectNo);
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(JsonResult.success(deckService.findDeck(deckVo.getProjectNo())));
+				.body(JsonResult.success(deckService.findDeck(projectNo)));
 	}
 	
+	@PostMapping("/create")
+	public ResponseEntity<JsonResult> create( @RequestBody DeckVo deckVo) {
+		System.out.println("data: "+deckVo);
+		boolean newVo = deckService.createDeck(deckVo);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(newVo));
+	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<JsonResult> updatDeck(@RequestBody DeckVo deckVo) {
+		System.out.println("deck update"+ deckVo);
+	System.out.println(deckService.updateDeck(deckVo));
+		
+		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.success(deckVo));
+	}
 
 }
