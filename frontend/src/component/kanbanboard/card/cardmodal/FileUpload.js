99515cd7e969e5790cf8a27da58a6416b8befb50
kwakdo <vv05044@gmail.com>
2022-08-31 15:15:07 +0900

    카드 수정 기능 cut

--- frontend/src/component/kanbanboard/card/cardmodal/FileUpload.js
+++ frontend/src/component/kanbanboard/card/cardmodal/FileUpload.js
@@ -1,17 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../../../../assets/css/cardFileUpload/cardFileUpload.css";
 import Upload from "./FileUploads/Upload";
+import { Modal } from 'react-bootstrap';
+import Button from '@mui/material/Button';
 
-class FileUpload extends Component {
-    render() {
+const FileUpload = ({show, setShow}) => {
+        const [page, setPage] = useState('card');
+        const handleClose = () => {
+            setShow(false);
+            setPage("card");
+        }
+    
         return (
-            <div>
-                <div className="Card2">
-                    <Upload />
-                </div>
-            </div>
+            <>
+                <Modal.Body>
+                    <div className="Card2" style={{height:"350px"}}>
+                        <Upload />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </>
         );
-    }
-}
+    };
 
 export default FileUpload;
